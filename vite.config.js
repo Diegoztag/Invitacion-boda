a14c355fbf6bd13b0a594cd6beb91506ec6affe1
Diego Zazueta <diego.zazueta@coppel.com>
2026-04-21 17:22:58 -0700

    refactor(frontend): se mueve la pagina landing a su propio directorio al igual que a sus archivos

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'frontend/index.html'),
+                main: resolve(__dirname, 'frontend/landing/index.html'),
                 invitation: resolve(__dirname, 'frontend/invitation/index.html'),
                 dashboard: resolve(__dirname, 'frontend/dashboard/index.html'),
                 login: resolve(__dirname, 'frontend/dashboard/login.html')
